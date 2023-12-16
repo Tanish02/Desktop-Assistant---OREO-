5a407fbbec112c62b92c888168c10952b1867624
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2023-12-16 06:48:19 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from config import apikey
 import datetime
 import random
 import numpy as np
-
+import pyttsx3
 
 chatStr = ""
+
 def chat(query):
     global chatStr
     print(chatStr)
@@ -16,7 +17,7 @@ def chat(query):
     chatStr += f"Tanish: {query}\n Oreo: "
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt= chatStr,
+        prompt=chatStr,
         temperature=0.7,
         max_tokens=256,
         top_p=1,
@@ -27,7 +28,6 @@ def chat(query):
     say(response["choices"][0]["text"])
     chatStr += f"{response['choices'][0]['text']}\n"
     return response["choices"][0]["text"]
-
 
 def ai(prompt):
     openai.api_key = apikey
@@ -47,17 +47,17 @@ def ai(prompt):
     if not os.path.exists("Openai"):
         os.mkdir("Openai")
 
-    # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
-    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip()}.txt", "w") as f:
         f.write(text)
 
 def say(text):
-    os.system(f'say "{text}"')
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        # r.pause_threshold =  0.6
         audio = r.listen(source)
         try:
             print("Recognizing...")
@@ -73,27 +73,28 @@ if __name__ == '__main__':
     while True:
         print("Listening...")
         query = takeCommand()
-        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"],
+                 ["google", "https://www.google.com"], ["facebook", "https://www.facebook.com"]]
         for site in sites:
             if f"Open {site[0]}".lower() in query.lower():
                 say(f"Opening {site[0]} sir...")
                 webbrowser.open(site[1])
 
-        if "open music" in query:
-            musicPath = " "
-            os.system(f"open {musicPath}")
+        if "play music" in query:
+            musicPath = r"C:\Users\GamerX\Desktop\music\1.mp3"
+            os.system(f"start {musicPath}")
 
-        elif "the time" in query:
-            musicPath = "    "
+        elif "time" in query:
             hour = datetime.datetime.now().strftime("%H")
-            min = datetime.datetime.now().strftime("%M")
-            say(f"Sir time is {hour} bajke {min} minutes")
+            minutes = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour}  {minutes} ")
 
         elif "open facetime".lower() in query.lower():
-            os.system(f"open /System/Applications/FaceTime.app")
+
+            os.system("start FaceTime")
 
         elif "open pass".lower() in query.lower():
-            os.system(f"open /Applications/Passky.app")
+            os.system("start Passky")
 
         elif "Using artificial intelligence".lower() in query.lower():
             ai(prompt=query)
@@ -107,15 +108,3 @@ if __name__ == '__main__':
         else:
             print("Chatting...")
             chat(query)
-
-
-
-
-
-
-
-
-
-
-
-
